866b8aae466d3605845ac08f18397b3800c5b130
Triston Armstrong <triston95strong@gmail.com>
2019-04-04 11:07:36 -0500

    Remove various text
    I accidentally left text in this file that was meant to help a friend understand how these things work with a comedic edge.

--- assignments/this.js
+++ assignments/this.js
@@ -1,16 +1,14 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. window binding - if this is global, it will refer to the window object ---- hard trying to convert the 
-                       information to my own words lol;
+* 1. window binding - if this is global, it will refer to the window object 
 
 * 2. implicit binding - this referes to the object a function is called on;
 
 * 3. new binding - if a constructor function is used. this referes to the newly created object that 
                     the constructor created;
 
-* 4. explicit binding - not really sure how to explain this without just saying... well, what it is lmfao so
-                         this is explicit when call or apply are used.... #reverse lmfao;
+* 4. explicit binding - this is explicit when call or apply are used.
 *
 * write out a code example of each explanation above
 */
@@ -18,40 +16,19 @@
 // Principle 1
 
 // // code example for Window Binding
-// this
+this
 
 // // Principle 2
 
 // // code example for Implicit Binding
-// Object.this
+Object.this
 
 // // Principle 3
 
 // // code example for New Binding
-// variable = new Object(stuff)
-// variable.stuff == this.stuff
+variable = new Object(stuff)
 // // Principle 4
 
 // // code example for Explicit Binding
-// Object.function.call(Object);
-// Object.function.apply(Object);
-
-
-
-
-
-
-
-
-// function Bitch(name, height, ass){
-//     this.name = name;
-//     this.height = height;
-//     this.ass = ass;
-// }
-// Bitch.prototype.clap = function(){
-//     return `${this.name}, clap that ${this.ass} ass`;
-// }
-
-// const efrain = new Bitch("efrain", "2inches", "fat");
-
-// console.log(efrain.clap())
+Object.function.call(Object);
+Object.function.apply(Object);
